9fd5e8a61a62c7095b1f4cffdcae88652dd4b8b6
Thong Phan <phanhongthong@gmail.com>
2019-09-01 16:09:07 +0700

    test fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,10 +10,16 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
   it(`should have as title 'marvel-thongphan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("marvel-thongphan");
+    expect(app.title).toEqual("marvel-thongphan1");
   });
 
   it("should render title in a h1 tag", () => {
