841bcade3771d2cb67263b2830c62c16d72697fc
Thong Phan <phanhongthong@gmail.com>
2019-09-01 16:21:40 +0700

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe("AppComponent", () => {
   it(`should have as title 'marvel-thongphan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("marvel-thongphan1");
+    expect(app.title).toEqual("marvel-thongphan");
   });
 
   it("should render title in a h1 tag", () => {
