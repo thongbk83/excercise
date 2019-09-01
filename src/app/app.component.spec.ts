e53582c6e63c6e81f2d69de677b4dbefa7e644cd
Thong Phan <phanhongthong@gmail.com>
2019-09-01 15:25:54 +0700

    fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,12 +10,6 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
   it(`should have as title 'marvel-thongphan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
